2b29bb380e11f8aedb9f2433310d785df38bb18a
notaphplover <roberto.pintos.lopez@gmail.com>
2022-01-18 09:57:40 +0100

    feat(packages): add duck-backend-env

---
+++ packages/duck-backend-env/lib/index.ts
@@ -0,0 +1,6 @@
+import { DotEnvLoader } from './common/adapter/dotenv/DotEnvLoader';
+import { EnvLoader } from './common/domain/EnvLoader';
+
+export type { EnvLoader };
+
+export { DotEnvLoader };
