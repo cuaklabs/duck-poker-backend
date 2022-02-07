78d910ed3a5a74cf7253d56503b259d9bd6042ee
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-08 00:02:49 +0100

    feat(testUtils): add  TestEnvDotEnvLoader

--- packages/testUtils/src/common/domain/model/TestEnv.ts
+++ packages/testUtils/src/common/domain/model/TestEnv.ts
@@ -1,4 +1,7 @@
 export interface TestEnv {
-  RESIS_HOST: string;
-  REDIS_PORT: string;
+  MONGODB_PASSWORD: string;
+  MONGODB_URL: string;
+  MONGODB_USERNAME: string;
+  REDIS_HOST: string;
+  REDIS_PORT: number;
 }
--- packages/testUtils/src/index.ts
+++ packages/testUtils/src/index.ts
@@ -1,6 +1,7 @@
 import { getTestEnvPath } from './common/domain/getTestEnvPath';
 import { TestEnv } from './common/domain/model/TestEnv';
+import { TestEnvDotEnvLoader } from './env/integration/dotenv/TestEnvDotEnvLoader';
 
 export type { TestEnv };
 
-export { getTestEnvPath };
+export { getTestEnvPath, TestEnvDotEnvLoader };
