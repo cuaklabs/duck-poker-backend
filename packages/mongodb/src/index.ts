08edbc9af35d07cc823a4300b9fa3f5877e8e48b
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-11 09:47:39 +0100

    feat(mongodb): update index with EntityMongoDbCreationAdapter and EntityMongoDbFindAdapter

--- packages/mongodb/src/index.ts
+++ packages/mongodb/src/index.ts
@@ -1,3 +1,7 @@
+import { EntityMongoDbCreationAdapter } from './common/integration/mongodb/adapter/EntityMongoDbCreationAdapter';
+import { EntityMongoDbFindAdapter } from './common/integration/mongodb/adapter/EntityMongoDbFindAdapter';
 import { BaseEntityMongoDb } from './common/integration/mongodb/model/BaseEntityMongoDb';
 
 export type { BaseEntityMongoDb };
+
+export { EntityMongoDbCreationAdapter, EntityMongoDbFindAdapter };
